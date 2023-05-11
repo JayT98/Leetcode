e7dee068490f16b53c54d557c4baa42b1ca0fe9b
JayT98 <kientrung2509@gmail.com>
2023-05-11 14:36:52 +1000

    update solution javascript and C# solution

--- maxFraction/maxFraction.js
+++ maxFraction/maxFraction.js
@@ -4,19 +4,35 @@
  * @return {number[]}
  */
 
+// Javascript Solution
 function maxFraction (numerators, denominators){
-    let max = 0;
-    let maxIndex = 0;
-    for (let i = 0; i < numerators.length; i++){
-        if (numerators[i]/denominators[i] > max){
-        max = numerators[i]/denominators[i];
-        maxIndex = i;
+    let maxFraction = 0;
+    for (let i = 1; i < numerators.length; i++){
+        if (numerators[maxFraction] * denominators[i] < numerators[i] * denominators[maxFraction]){
+            maxFraction = i;
         }
     }
-    return [numerators[maxIndex], denominators[maxIndex]];
+    
+    return maxFraction;
 }
 
-console.log(maxFraction([5,2,5], [6,3,4]));
+/* C# Solution */
+// int MaxFraction(int[] numerators, int[] denominators)
+// {
+//     int cs = 0;
 
+//     for (int i= 1; i < numerators.Length; i++){
+//         if(numerators[cs] * denominators[i] < numerators[i] * denominators[cs]){
+//             cs = i;
+//         }
+//     }
+//     return cs;
+// }
 
+// Test Case
+
+console.log(maxFraction([5,2,5], [6,3,4])); //2
+console.log(maxFraction([1,2,3,10], [1,3,4,11])); //0
+console.log(maxFraction([1,2], [4,3])); // 1
+console.log(maxFraction([1,9,2,5], [4,2,3,6])); // 1
 
